f6f9acedd9332524697422294ecf091bde8a33c7
CK Tan <cktan@vitessedata.com>
2016-07-16 13:17:23 -0700

    add Makefiles

---
+++ libhdfs3/src/src.mk
@@ -0,0 +1,12 @@
+CXXFLAGS += -I/home/cktan/p/tool2/installed/include 
+CXXFLAGS += -I/home/cktan/p/tool2/installed/include/libxml2
+CXXFLAGS += -I../common -I.. -I../proto
+CXXFLAGS += -Wall -msse4.2 -std=c++11 -g
+CXXFLAGS += -O0
+#CXXFLAGS += -O2 -g -DNDEBUG
+
+all: $(OBJS)
+	ld -r $(OBJS) -o subobj.o
+
+clean:
+	rm -f $(GENFILES) $(OBJS) *.o
